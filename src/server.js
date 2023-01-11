fa49f694467e6c613d99a8fd26a113f0ce52a8a3
pedro <pedroextone123@gmail.com>
2023-01-10 21:50:30 -0300

    estrutura inicial do prejeto

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+//const path = require('path')
+
+//const db = require('./database')
+const routes = require('./routes/routes')
+
+const app = express()
+
+//db.connect()
+
+//habilita server para receber dados via post (form)
+app.use(express.urlencoded({extended: true}))
+
+//definindo as rotas
+app.use('/api', routes)
+
+//executando o servidor
+const port = process.env.PORT || 8080
+app.listen(port, ()=> console.log(`server is listening on port ${port}`))
