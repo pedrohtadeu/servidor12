61ff055415cf371f5fca96967beae9c828fd22d8
pedro <pedroextone123@gmail.com>
2023-01-14 02:24:37 -0300

    primeiro commit

---
+++ src/controllers/products.js
@@ -0,0 +1,16 @@
+const ProductsModel = require('../models/products')
+
+async  function get(req, res){
+    const {id} =  req.params 
+
+    const obj = id ? {_id: id} : null
+
+    const products = await ProductsModel.find(obj)
+
+    res.send(products)
+    
+}
+
+module.exports= {
+    get,
+}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,17 @@
 const router = require('express').Router()
+const ProductController = require('../controllers/products')
 
-router.get('/clientes', ( req, res)=> {
-    res.send({
-        ok: 123
-    })
-})
 
+//verbos http (4 tip0)
+//get - receber dados
+//post - enviar/receber dados
+//put- atualizar dados
+//delete - deletar
+
+
+
+router.get('/products/:id?', ProductController.get)
+//router.post('/products', ProductController.post)
+//router.put('/products/:id', ProductController.put)
+//router.delete('/products/:id', ProductController.delete)
 module.exports = router
